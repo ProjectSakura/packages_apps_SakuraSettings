8b1a62a6d723ef3e4b46d4c6b9b3f5b84598413e
Laker87 <lakerman1987@gmail.com>
2021-04-24 08:08:56 +0200

    Sakura: Add screenshot quality setting [2/2]
    Signed-off-by: LordShenron <shen.priyanshu@gmail.com>

--- src/com/sakura/settings/fragments/MiscSettings.java
+++ src/com/sakura/settings/fragments/MiscSettings.java
@@ -66,6 +66,8 @@ public class MiscSettings extends SettingsPreferenceFragment implements
                 Settings.System.SCREENSHOT_SOUND, 1, UserHandle.USER_CURRENT);
         Settings.System.putIntForUser(resolver,
                 Settings.System.AUDIO_PANEL_VIEW_TIMEOUT, 3, UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(resolver,
+                Settings.System.SCREENSHOT_COMPRESSION, 100, UserHandle.USER_CURRENT);
         GamingMode.reset(mContext);
     }
 
