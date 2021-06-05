fb079c411191890862c425fdd01e5c78adf4d8e9
Gnonymous7 <geraldfebrinzaghi7@gmail.com>
2021-06-05 15:52:11 +0200

    [SQUASH]Sakura: Remove sakura themes and move gvisual mod to separate fragment
    - We have color bucket so kill sakura themes
    - Move gvisual mod to it's own section to keep interface fragment clean
    
    co-authored-by: Genkzsz11 <Genkzsz11@gmail.com>
    Signed-off-by: LordShenron <shen.priyanshu@gmail.com>

--- src/com/sakura/settings/fragments/InterfaceSettings.java
+++ src/com/sakura/settings/fragments/InterfaceSettings.java
@@ -31,50 +31,25 @@ import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.content.om.IOverlayManager;
-import android.content.om.OverlayInfo;
-import android.content.pm.PackageManager;
-import android.os.ServiceManager;
-import android.app.UiModeManager;
-import android.os.RemoteException;
+
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.internal.logging.nano.MetricsProto;
 
-import androidx.preference.PreferenceManager;
-import java.util.Objects;
-
-import com.android.internal.util.sakura.ThemesUtils;
-import com.android.internal.util.sakura.Utils;
 import com.sakura.settings.utils.TelephonyUtils;
-
-import static android.os.UserHandle.USER_SYSTEM;
-import android.app.UiModeManager;
-
 import com.android.settings.R;
 
 public class InterfaceSettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
 
-    private static final String PREF_THEME_SWITCH = "theme_switch";
-    private static final String PREF_ROUNDED_CORNER = "rounded_ui";
-    private static final String GVISUAL_FOOTER = "gvisual_footer";
-    private static final String PREF_SB_HEIGHT = "statusbar_height";
     private static final String KEY_SHOW_ROAMING = "roaming_indicator_icon";
     private static final String KEY_SHOW_FOURG = "show_fourg_icon";
     private static final String KEY_SHOW_DATA_DISABLED = "data_disabled_icon";
     private static final String KEY_USE_OLD_MOBILETYPE = "use_old_mobiletype";
 
-    private UiModeManager mUiModeManager;
-    private ListPreference mThemeSwitch;
-    private ListPreference mRoundedUi;
-    private ListPreference mSbHeight;
     private SwitchPreference mShowRoaming;
     private SwitchPreference mShowFourg;
     private SwitchPreference mDataDisabled;
     private SwitchPreference mOldMobileType;
-
-    private IOverlayManager mOverlayService;
-    private IOverlayManager mOverlayManager;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -83,33 +58,7 @@ public class InterfaceSettings extends SettingsPreferenceFragment implements
         ContentResolver resolver = getActivity().getContentResolver();
         Context mContext = getActivity().getApplicationContext();
         final PreferenceScreen prefScreen = getPreferenceScreen();
-        findPreference(GVISUAL_FOOTER).setTitle(R.string.gvisual_footer);
-
-        mUiModeManager = getContext().getSystemService(UiModeManager.class);
-        mOverlayService = IOverlayManager.Stub
-                .asInterface(ServiceManager.getService(Context.OVERLAY_SERVICE));
-	setupThemeSwitchPref();
-
-        mRoundedUi = (ListPreference) findPreference(PREF_ROUNDED_CORNER);
-        int roundedValue = getOverlayPosition(ThemesUtils.UI_RADIUS);
-        if (roundedValue != -1) {
-            mRoundedUi.setValue(String.valueOf(roundedValue + 2));
-        } else {
-            mRoundedUi.setValue("1");
-        }
-        mRoundedUi.setSummary(mRoundedUi.getEntry());
-        mRoundedUi.setOnPreferenceChangeListener(this);
-
-        mSbHeight = (ListPreference) findPreference(PREF_SB_HEIGHT);
-        int sbHeightValue = getOverlayPosition(ThemesUtils.STATUSBAR_HEIGHT);
-        if (sbHeightValue != -1) {
-            mSbHeight.setValue(String.valueOf(sbHeightValue + 2));
-        } else {
-            mSbHeight.setValue("1");
-        }
-        mSbHeight.setSummary(mSbHeight.getEntry());
-        mSbHeight.setOnPreferenceChangeListener(this);
-
+        
         mShowRoaming = (SwitchPreference) findPreference(KEY_SHOW_ROAMING);
         mShowFourg = (SwitchPreference) findPreference(KEY_SHOW_FOURG);
         mDataDisabled = (SwitchPreference) findPreference(KEY_SHOW_DATA_DISABLED);
@@ -128,52 +77,8 @@ public class InterfaceSettings extends SettingsPreferenceFragment implements
                     mConfigUseOldMobileType ? 1 : 0, UserHandle.USER_CURRENT) != 0;
             mOldMobileType.setChecked(showing);
         }
+	}
 
-    }
-
-    private void setupThemeSwitchPref() {
-        mThemeSwitch = (ListPreference) findPreference(PREF_THEME_SWITCH);
-        mThemeSwitch.setOnPreferenceChangeListener(this);
-        if (Utils.isThemeEnabled("com.android.system.theme.mizu")) {
-            mThemeSwitch.setValue("8");
-        } else if (Utils.isThemeEnabled("com.android.theme.materialocean.system")) {
-            mThemeSwitch.setValue("7");
-        } else if (Utils.isThemeEnabled("com.android.theme.pitchblack.system")) {
-            mThemeSwitch.setValue("6");
-        } else if (Utils.isThemeEnabled("com.android.theme.chocox.system")) {
-            mThemeSwitch.setValue("5");
-        } else if (Utils.isThemeEnabled("com.android.theme.bakedgreen.system")) {
-            mThemeSwitch.setValue("4");
-        } else if (Utils.isThemeEnabled("com.android.theme.solarizeddark.system")) {
-            mThemeSwitch.setValue("3");
-        } else if (mUiModeManager.getNightMode() == UiModeManager.MODE_NIGHT_YES) {
-            mThemeSwitch.setValue("2");
-        } else {
-            mThemeSwitch.setValue("1");
-        }
-    }
-
-    private void handleBackgrounds(Boolean state, Context context, int mode, String[] overlays) {
-        if (context != null) {
-            Objects.requireNonNull(context.getSystemService(UiModeManager.class))
-                    .setNightMode(mode);
-        }
-        for (int i = 0; i < overlays.length; i++) {
-            String background = overlays[i];
-            try {
-                mOverlayService.setEnabled(background, state, USER_SYSTEM);
-            } catch (RemoteException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    public void handleOverlays(String packagename, Boolean state, IOverlayManager mOverlayManager) {
-        try {
-            mOverlayService.setEnabled(packagename, state, USER_SYSTEM);
-        } catch (RemoteException e) {
-            e.printStackTrace();
-        }
     }
 
     @Override
@@ -211,136 +116,6 @@ public class InterfaceSettings extends SettingsPreferenceFragment implements
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-    if (preference == mThemeSwitch) {
-            String theme_switch = (String) newValue;
-            final Context context = getContext();
-            switch (theme_switch) {
-                case "1":
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_NO, ThemesUtils.SOLARIZED_DARK);
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_NO, ThemesUtils.BAKED_GREEN);
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_NO, ThemesUtils.CHOCO_X);
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_NO, ThemesUtils.PITCH_BLACK);
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_NO, ThemesUtils.MATERIAL_OCEAN);
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_NO, ThemesUtils.MIZU_THEMES);
-                    break;
-                case "2":
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.SOLARIZED_DARK);
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.BAKED_GREEN);
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.CHOCO_X);
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.PITCH_BLACK);
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.MATERIAL_OCEAN);
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.MIZU_THEMES);
-                    break;
-                case "3":
-                    handleBackgrounds(true, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.SOLARIZED_DARK);
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.BAKED_GREEN);
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.CHOCO_X);
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.PITCH_BLACK);
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.MATERIAL_OCEAN);
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.MIZU_THEMES);
-                    break;
-                case "4":
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.SOLARIZED_DARK);
-                    handleBackgrounds(true, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.BAKED_GREEN);
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.CHOCO_X);
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.PITCH_BLACK);
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.MATERIAL_OCEAN);
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.MIZU_THEMES);
-                    break;
-                case "5":
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.SOLARIZED_DARK);
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.BAKED_GREEN);
-                    handleBackgrounds(true, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.CHOCO_X);
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.PITCH_BLACK);
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.MATERIAL_OCEAN);
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.MIZU_THEMES);
-                    break;
-                case "6":
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.SOLARIZED_DARK);
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.BAKED_GREEN);
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.CHOCO_X);
-                    handleBackgrounds(true, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.PITCH_BLACK);
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.MATERIAL_OCEAN);
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.MIZU_THEMES);
-                    break;
-                case "7":
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.SOLARIZED_DARK);
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.BAKED_GREEN);
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.CHOCO_X);
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.PITCH_BLACK);
-                    handleBackgrounds(true, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.MATERIAL_OCEAN);
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.MIZU_THEMES);
-                    break;
-                case "8":
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.SOLARIZED_DARK);
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.BAKED_GREEN);
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.CHOCO_X);
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.PITCH_BLACK);
-                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.MATERIAL_OCEAN);
-                    handleBackgrounds(true, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.MIZU_THEMES);
-                    break;
-           }
-            try {
-                 mOverlayService.reloadAndroidAssets(UserHandle.USER_CURRENT);
-                 mOverlayService.reloadAssets("com.android.settings", UserHandle.USER_CURRENT);
-                 mOverlayService.reloadAssets("com.android.systemui", UserHandle.USER_CURRENT);
-             } catch (RemoteException ignored) {
-             }
-            return true;
-            }
-            else if (preference == mRoundedUi) {
-            String rounded = (String) newValue;
-            int roundedValue = Integer.parseInt(rounded);
-            mRoundedUi.setValue(String.valueOf(roundedValue));
-            String overlayName = getOverlayName(ThemesUtils.UI_RADIUS);
-                if (overlayName != null) {
-                    handleOverlays(overlayName, false, mOverlayManager);
-                }
-                if (roundedValue > 1) {
-                    handleOverlays(ThemesUtils.UI_RADIUS[roundedValue -2],
-                            true, mOverlayManager);
-            }
-            mRoundedUi.setSummary(mRoundedUi.getEntry());
-            return true;
-            } else if (preference == mSbHeight) {
-            String sbheight = (String) newValue;
-            int sbheightValue = Integer.parseInt(sbheight);
-            mSbHeight.setValue(String.valueOf(sbheightValue));
-            String overlayName = getOverlayName(ThemesUtils.STATUSBAR_HEIGHT);
-                if (overlayName != null) {
-                    handleOverlays(overlayName, false, mOverlayManager);
-                }
-                if (sbheightValue > 1) {
-                    handleOverlays(ThemesUtils.STATUSBAR_HEIGHT[sbheightValue -2],
-                            true, mOverlayManager);
-            }
-            mSbHeight.setSummary(mSbHeight.getEntry());
-            return true;
-            }
-            return false;
-       }
-
-
-    private int getOverlayPosition(String[] overlays) {
-        int position = -1;
-        for (int i = 0; i < overlays.length; i++) {
-            String overlay = overlays[i];
-            if (Utils.isThemeEnabled(overlay)) {
-                position = i;
-            }
-        }
-        return position;
+        return false;
     }
-
-    private String getOverlayName(String[] overlays) {
-        String overlayName = null;
-        for (int i = 0; i < overlays.length; i++) {
-            String overlay = overlays[i];
-            if (Utils.isThemeEnabled(overlay)) {
-                overlayName = overlay;
-            }
-        }
-        return overlayName;
-    }
-
 }
