abb93382bf4016af531297fc70d5b2ad35280aa3
Pranav Vashi <neobuddy89@gmail.com>
2024-04-29 09:04:35 +0900

    Sakura Settings: Network traffic mode for status bar [3/3]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: Id540c34fc53851203495680dacf93b08098b4ce0
    Signed-off-by: AnierinB <anierin@evolution-x.org>

---
+++ src/org/sakura/settings/fragments/statusbar/NetworkTrafficSettings.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2017-2024 crDroid Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.sakura.settings.fragments.statusbar;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.os.Bundle;
+import android.os.UserHandle;
+
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.settings.R;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settingslib.search.SearchIndexable;
+
+import java.util.List;
+
+import lineageos.providers.LineageSettings;
+
+@SearchIndexable
+public class NetworkTrafficSettings extends SettingsPreferenceFragment {
+
+    private static final String TAG = "NetworkTrafficSettings";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.network_traffic_settings);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.SAKURA_SETTINGS;
+    }
+
+    public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+        new BaseSearchIndexProvider(R.xml.network_traffic_settings) {
+
+            @Override
+            public List<String> getNonIndexableKeys(Context context) {
+                List<String> keys = super.getNonIndexableKeys(context);
+                return keys;
+            }
+        };
+}
