20ea4e67e88035848b61549ff5b5993b2482a07f
Pranav Vashi <neobuddy89@gmail.com>
2024-05-12 21:01:18 +0900

    Sakura Settings: Allow using 4G icon instead LTE [2/2]
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- src/org/sakura/settings/fragments/statusbar/StatusBar.java
+++ src/org/sakura/settings/fragments/statusbar/StatusBar.java
@@ -39,6 +39,7 @@ public class StatusBar extends SettingsPreferenceFragment implements
     private static final String KEY_ICONS_CATEGORY = "status_bar_icons_category";
     private static final String KEY_DATA_DISABLED_ICON = "data_disabled_icon";
     private static final String KEY_BLUETOOTH_BATTERY_STATUS = "bluetooth_show_battery";
+    private static final String KEY_FOUR_G_ICON = "show_fourg_icon";
 
     private static final int PULLDOWN_DIR_NONE = 0;
     private static final int PULLDOWN_DIR_RIGHT = 1;
@@ -49,6 +50,7 @@ public class StatusBar extends SettingsPreferenceFragment implements
 
     private PreferenceCategory mIconsCategory;
     private SystemSettingSwitchPreference mDataDisabledIcon;
+    private SystemSettingSwitchPreference mFourgIcon;
     private SystemSettingSwitchPreference mBluetoothBatteryStatus;
 
     @Override
@@ -68,6 +70,7 @@ public class StatusBar extends SettingsPreferenceFragment implements
 
         mIconsCategory = (PreferenceCategory) findPreference(KEY_ICONS_CATEGORY);
         mDataDisabledIcon = (SystemSettingSwitchPreference) findPreference(KEY_DATA_DISABLED_ICON);
+        mFourgIcon = (SystemSettingSwitchPreference) findPreference(KEY_FOUR_G_ICON);
         mBluetoothBatteryStatus = (SystemSettingSwitchPreference) findPreference(KEY_BLUETOOTH_BATTERY_STATUS);
 
         if (getResources().getConfiguration().getLayoutDirection() == View.LAYOUT_DIRECTION_RTL) {
@@ -77,6 +80,7 @@ public class StatusBar extends SettingsPreferenceFragment implements
 
         if (!DeviceUtils.deviceSupportsMobileData(context)) {
             mIconsCategory.removePreference(mDataDisabledIcon);
+            mIconsCategory.removePreference(mFourgIcon);
         }
 
         if (!DeviceUtils.deviceSupportsBluetooth(context)) {
@@ -136,6 +140,7 @@ public class StatusBar extends SettingsPreferenceFragment implements
 
                 if (!DeviceUtils.deviceSupportsMobileData(context)) {
                     keys.add(KEY_DATA_DISABLED_ICON);
+                    keys.add(KEY_FOUR_G_ICON);
                 }
                 if (!DeviceUtils.deviceSupportsBluetooth(context)) {
                     keys.add(KEY_BLUETOOTH_BATTERY_STATUS);
