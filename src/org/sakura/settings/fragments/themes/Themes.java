9b1cbabf43b8ebcd5f4317e951dae5743f39b2e2
SagarMakhar <sagarmakhar@gmail.com>
2024-05-13 22:55:58 +0000

    Sakura Settings: UDFPS animations
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- src/org/sakura/settings/fragments/themes/Themes.java
+++ src/org/sakura/settings/fragments/themes/Themes.java
@@ -8,6 +8,7 @@ package org.sakura.settings.fragments.themes;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.res.Resources;
+import android.hardware.fingerprint.FingerprintManager;
 import android.os.Bundle;
 
 import androidx.preference.Preference;
@@ -16,6 +17,7 @@ import androidx.preference.PreferenceCategory;
 import androidx.preference.PreferenceScreen;
 
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.internal.util.sakura.Utils;
 import com.android.settings.R;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.SettingsPreferenceFragment;
@@ -34,10 +36,14 @@ public class Themes extends SettingsPreferenceFragment implements
     private static final String KEY_ICONS_CATEGORY = "themes_icons_category";
     private static final String KEY_NAVBAR_ICON = "android.theme.customization.navbar";
     private static final String KEY_SIGNAL_ICON = "android.theme.customization.signal_icon";
+    private static final String KEY_ANIMATIONS_CATEGORY = "themes_animations_category";
+    private static final String KEY_UDFPS_ANIMATION = "udfps_animation";
 
     private PreferenceCategory mIconsCategory;
     private Preference mNavbarIcon;
     private Preference mSignalIcon;
+    private PreferenceCategory mAnimationsCategory;
+    private Preference mUdfpsAnimation;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -52,6 +58,8 @@ public class Themes extends SettingsPreferenceFragment implements
         mIconsCategory = (PreferenceCategory) findPreference(KEY_ICONS_CATEGORY);
         mNavbarIcon = (Preference) findPreference(KEY_NAVBAR_ICON);
         mSignalIcon = (Preference) findPreference(KEY_SIGNAL_ICON);
+        mAnimationsCategory = (PreferenceCategory) findPreference(KEY_ANIMATIONS_CATEGORY);
+        mUdfpsAnimation = (Preference) findPreference(KEY_UDFPS_ANIMATION);
 
         if (!DeviceUtils.deviceSupportsMobileData(context)) {
             mIconsCategory.removePreference(mSignalIcon);
@@ -59,6 +67,17 @@ public class Themes extends SettingsPreferenceFragment implements
 
         if (DeviceUtils.isEdgeToEdgeEnabled(context)) {
             mIconsCategory.removePreference(mNavbarIcon);
+        }
+
+        FingerprintManager fingerprintManager = (FingerprintManager)
+                getActivity().getSystemService(Context.FINGERPRINT_SERVICE);
+
+        if (fingerprintManager == null || !fingerprintManager.isHardwareDetected()) {
+            mAnimationsCategory.removePreference(mUdfpsAnimation);
+        } else {
+            if (!Utils.isPackageInstalled(context, "org.sakura.udfps.animations")) {
+                mAnimationsCategory.removePreference(mUdfpsAnimation);
+            }
         }
     }
 
@@ -82,8 +101,19 @@ public class Themes extends SettingsPreferenceFragment implements
                 List<String> keys = super.getNonIndexableKeys(context);
                 final Resources resources = context.getResources();
 
+                FingerprintManager fingerprintManager = (FingerprintManager)
+                        context.getSystemService(Context.FINGERPRINT_SERVICE);
+
                 if (!DeviceUtils.deviceSupportsMobileData(context)) {
                     keys.add(KEY_SIGNAL_ICON);
+                }
+
+                if (fingerprintManager == null || !fingerprintManager.isHardwareDetected()) {
+                    keys.add(KEY_UDFPS_ANIMATION);
+                } else {
+                    if (!Utils.isPackageInstalled(context, "org.sakura.udfps.animations")) {
+                        keys.add(KEY_UDFPS_ANIMATION);
+                    }
                 }
                 return keys;
             }
