5a10dc91104fc8e4bba433a939f3741372e7d012
Pranav Vashi <neobuddy89@gmail.com>
2024-05-22 16:44:08 +0900

    Sakura Settings: Fingerprint authentication vibration [2/2]
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- src/org/sakura/settings/fragments/lockscreen/LockScreen.java
+++ src/org/sakura/settings/fragments/lockscreen/LockScreen.java
@@ -36,6 +36,8 @@ public class LockScreen extends SettingsPreferenceFragment implements
     private static final String KEY_FINGERPRINT_CATEGORY = "lock_screen_fingerprint_category";
     private static final String KEY_RIPPLE_EFFECT = "enable_ripple_effect";
     private static final String KEY_SCREEN_OFF_UDFPS = "screen_off_udfps_enabled";
+    private static final String KEY_AUTHENTICATION_SUCCESS = "fp_success_vibrate";
+    private static final String KEY_AUTHENTICATION_ERROR = "fp_error_vibrate";
 
     private PreferenceCategory mFingerprintCategory;
     private SecureSettingSwitchPreference mScreenOffUdfps;
@@ -96,6 +98,8 @@ public class LockScreen extends SettingsPreferenceFragment implements
                 if (fingerprintManager == null || !fingerprintManager.isHardwareDetected()) {
                     keys.add(KEY_RIPPLE_EFFECT);
                     keys.add(KEY_SCREEN_OFF_UDFPS);
+                    keys.add(KEY_AUTHENTICATION_SUCCESS);
+                    keys.add(KEY_AUTHENTICATION_ERROR);
                 } else {
                     boolean screenOffUdfpsAvailable = resources.getBoolean(
                         com.android.internal.R.bool.config_supportScreenOffUdfps) ||
