b0a2e9bcb9bdd8aa3b59bf6e3ddd76994eca7e94
Adin Kwok <adin@adin.dev>
2025-01-23 03:06:33 +0900

    Sakura Settings: Smart Pixels [2/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/org/sakura/settings/fragments/miscellaneous/Miscellaneous.java
+++ src/org/sakura/settings/fragments/miscellaneous/Miscellaneous.java
@@ -5,6 +5,7 @@
 
 package org.sakura.settings.fragments.miscellaneous;
 
+import android.app.Activity;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.res.Resources;
@@ -23,21 +24,37 @@ import com.android.settingslib.search.SearchIndexable;
 
 import java.util.List;
 
+import org.sakura.settings.fragments.miscellaneous.SmartPixels;
+
 @SearchIndexable
 public class Miscellaneous extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
 
     private static final String TAG = "Miscellaneous";
 
+    private static final String KEY_DEV_CATEGORY = "miscellaneous_developer_options_category";
+    private static final String KEY_SMART_PIXELS = "smart_pixels";
+
+    private PreferenceCategory mDevOptionsCategory;
+    private Preference mSmartPixels;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.sakura_settings_miscellaneous);
 
-        final Context context = getContext();
-        final ContentResolver resolver = context.getContentResolver();
+        Context mContext = getActivity().getApplicationContext();
+        final ContentResolver resolver = mContext.getContentResolver();
         final PreferenceScreen prefScreen = getPreferenceScreen();
-        final Resources resources = context.getResources();
+        final Resources res = mContext.getResources();
+
+        mDevOptionsCategory = (PreferenceCategory) findPreference(KEY_DEV_CATEGORY);
+        mSmartPixels = (Preference) findPreference(KEY_SMART_PIXELS);
+        boolean mSmartPixelsSupported = getResources().getBoolean(
+                com.android.internal.R.bool.config_supportSmartPixels);
+        if (!mSmartPixelsSupported) {
+            mDevOptionsCategory.removePreference(mSmartPixels);
+        }
     }
 
     @Override
@@ -55,11 +72,12 @@ public class Miscellaneous extends SettingsPreferenceFragment implements
     public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
         new BaseSearchIndexProvider(R.xml.sakura_settings_miscellaneous) {
 
-            @Override
-            public List<String> getNonIndexableKeys(Context context) {
-                List<String> keys = super.getNonIndexableKeys(context);
-                final Resources resources = context.getResources();
-                return keys;
-            }
-        };
+                    boolean mSmartPixelsSupported = context.getResources().getBoolean(
+                            com.android.internal.R.bool.config_supportSmartPixels);
+                    if (!mSmartPixelsSupported)
+                        keys.add(KEY_SMART_PIXELS);
+
+                    return keys;
+                }
+            };
 }
