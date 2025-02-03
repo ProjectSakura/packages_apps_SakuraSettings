1a1e4580898c02cdfe87635dd9e8b99597894b03
Joey Huab <jhuab@edukasyon.ph>
2025-02-04 07:50:03 +0900

    Sakura Settings: Show restart SystemUI dialog for some toggles
    * For Compact HUN and Colored status bar icons

--- src/org/sakura/settings/fragments/notifications/Notifications.java
+++ src/org/sakura/settings/fragments/notifications/Notifications.java
@@ -24,7 +24,9 @@ import com.android.settingslib.search.SearchIndexable;
 
 import java.util.List;
 
+import org.sakura.settings.preferences.SystemPropertySwitchPreference;
 import org.sakura.settings.preferences.SystemSettingSwitchPreference;
+import org.sakura.settings.utils.SystemUtils;
 
 @SearchIndexable
 public class Notifications extends SettingsPreferenceFragment implements
@@ -34,9 +36,11 @@ public class Notifications extends SettingsPreferenceFragment implements
 
     private static final String KEY_ALERT_SLIDER_PREF = "alert_slider_notifications";
     private static final String KEY_INTERFACE_CATEGORY = "notifications_interface_category";
+    private static final String KEY_COMPACT_HUN = "persist.sys.compact_heads_up_notification.always_show";
 
     private PreferenceCategory mInterfaceCategory;
     private Preference mAlertSlider;
+    private SystemPropertySwitchPreference mCompactHUN;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -55,12 +59,19 @@ public class Notifications extends SettingsPreferenceFragment implements
         if (!mAlertSliderAvailable) {
             mInterfaceCategory.removePreference(mAlertSlider);
         }
+
+        mCompactHUN = (SystemPropertySwitchPreference) findPreference(KEY_COMPACT_HUN);
+        mCompactHUN.setOnPreferenceChangeListener(this);
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         final Context context = getContext();
         final ContentResolver resolver = context.getContentResolver();
+        if (preference == mCompactHUN) {
+            SystemUtils.showSystemUiRestartDialog(context);
+            return true;
+        }
         return false;
     }
 
--- src/org/sakura/settings/fragments/statusbar/StatusBar.java
+++ src/org/sakura/settings/fragments/statusbar/StatusBar.java
@@ -31,6 +31,7 @@ import lineageos.preference.LineageSystemSettingListPreference;
 import org.sakura.settings.preferences.SystemSettingListPreference;
 import org.sakura.settings.preferences.SystemSettingSwitchPreference;
 import org.sakura.settings.utils.DeviceUtils;
+import org.sakura.settings.utils.SystemUtils;
 
 @SearchIndexable
 public class StatusBar extends SettingsPreferenceFragment implements
@@ -47,6 +48,7 @@ public class StatusBar extends SettingsPreferenceFragment implements
     private static final String KEY_DATA_DISABLED_ICON = "data_disabled_icon";
     private static final String KEY_BLUETOOTH_BATTERY_STATUS = "bluetooth_show_battery";
     private static final String KEY_FOUR_G_ICON = "show_fourg_icon";
+    private static final String KEY_COLORED_ICONS = "statusbar_colored_icons";
 
     private static final int PULLDOWN_DIR_NONE = 0;
     private static final int PULLDOWN_DIR_RIGHT = 1;
@@ -65,6 +67,7 @@ public class StatusBar extends SettingsPreferenceFragment implements
     private SystemSettingSwitchPreference mDataDisabledIcon;
     private SystemSettingSwitchPreference mFourgIcon;
     private SystemSettingSwitchPreference mBluetoothBatteryStatus;
+    private SystemSettingSwitchPreference mColoredIcons;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -89,6 +92,8 @@ public class StatusBar extends SettingsPreferenceFragment implements
         mDataDisabledIcon = (SystemSettingSwitchPreference) findPreference(KEY_DATA_DISABLED_ICON);
         mFourgIcon = (SystemSettingSwitchPreference) findPreference(KEY_FOUR_G_ICON);
         mBluetoothBatteryStatus = (SystemSettingSwitchPreference) findPreference(KEY_BLUETOOTH_BATTERY_STATUS);
+        mColoredIcons = (SystemSettingSwitchPreference) findPreference(KEY_COLORED_ICONS);
+        mColoredIcons.setOnPreferenceChangeListener(this);
 
         if (getResources().getConfiguration().getLayoutDirection() == View.LAYOUT_DIRECTION_RTL) {
             mQuickPulldown.setEntries(R.array.status_bar_quick_pull_down_entries_rtl);
@@ -142,6 +147,9 @@ public class StatusBar extends SettingsPreferenceFragment implements
                     Settings.System.STATUS_BAR_BATTERY_STYLE, BATTERY_STYLE_PORTRAIT, UserHandle.USER_CURRENT);
             mBatteryTextCharging.setEnabled(batterystyle == BATTERY_STYLE_HIDDEN ||
                     (batterystyle != BATTERY_STYLE_TEXT && value != 2));
+            return true;
+        } else if (preference == mColoredIcons) {
+            SystemUtils.showSystemUiRestartDialog(context);
             return true;
         }
         return false;
