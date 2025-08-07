18821033dbef2632cd539500770439618f284741
Ido Ben-Hur <idoybh2@gmail.com>
2025-08-07 23:09:39 +0900

    Sakura Settings: MonetSettings: Manually set default values to preferences

--- src/org/sakura/settings/fragments/themes/MonetSettings.java
+++ src/org/sakura/settings/fragments/themes/MonetSettings.java
@@ -215,6 +215,16 @@ public class MonetSettings extends DashboardFragment implements
                 mWholePalettePref.setChecked(wholePalette);
                 mTintBackgroundPref.setChecked(tintBG);
             } catch (JSONException | IllegalArgumentException ignored) {}
+        } else {
+            // reflect default values in every preference
+            mThemeStylePref.setValueIndex(0);
+            mColorSourcePref.setValueIndex(0);
+            mLuminancePref.setValue(0);
+            mChromaPref.setValue(0);
+            mAccentBackgroundPref.setChecked(false);
+            mWholePalettePref.setChecked(false);
+            mTintBackgroundPref.setChecked(false);
+            updateAccentEnablement("both");
         }
     }
 
