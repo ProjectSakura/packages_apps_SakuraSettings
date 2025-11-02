251030648015d9aa903c7d28711a9d5725f6cec7
Hexaboo <hexaboo@outlook.com>
2025-11-02 05:30:13 +0100

    SuperiorLab: Import Statusbar settings from LineageParts
    [Joker-V2]
    1, Adapt to SuperiorLab
    2, Partially revert https://github.com/Joker-V2/packages_apps_SuperiorLab/commit/e9cb90c8a63b5ac72207270ec71fdeae72b99f4a
    
    Signed-off-by: Joker-V2 <nipinna0@gmail.com>

--- src/com/superior/lab/fragments/StatusBarSettings.java
+++ src/com/superior/lab/fragments/StatusBarSettings.java
@@ -16,62 +16,179 @@
 
 package com.superior.lab.fragments;
 
-import com.android.internal.logging.nano.MetricsProto;
-
-import android.os.Bundle;
+import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
-import android.os.UserHandle;
-import android.content.ContentResolver;
-import android.content.res.Resources;
-import androidx.preference.ListPreference;
-import androidx.preference.Preference;
-import androidx.preference.PreferenceGroup;
-import androidx.preference.PreferenceScreen;
-import androidx.preference.PreferenceCategory;
-import androidx.preference.Preference.OnPreferenceChangeListener;
-import androidx.preference.PreferenceFragment;
-import androidx.preference.SwitchPreference;
+import android.os.BatteryManager;
+import android.os.Bundle;
 import android.provider.Settings;
-import com.android.settings.R;
-
-import java.util.Locale;
 import android.text.TextUtils;
+import android.text.format.DateFormat;
 import android.view.View;
 
-import com.android.settings.SettingsPreferenceFragment;
-import com.android.settings.Utils;
-import android.util.Log;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.PreferenceScreen;
 
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Map;
-import java.util.HashMap;
-import java.util.Collections;
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settingslib.fuelgauge.BatteryUtils;
+
+import lineageos.preference.LineageSystemSettingListPreference;
+
+import com.superior.lab.utils.DeviceUtils;
 
 public class StatusBarSettings extends SettingsPreferenceFragment implements
-        OnPreferenceChangeListener {
+        Preference.OnPreferenceChangeListener {
+
+    private static final String CATEGORY_BATTERY = "status_bar_battery_key";
+    private static final String CATEGORY_CLOCK = "status_bar_clock_key";
+    private static final String ICON_BLACKLIST = "icon_blacklist";
+    private static final String STATUS_BAR_CLOCK_STYLE = "status_bar_clock";
+    private static final String STATUS_BAR_AM_PM = "status_bar_am_pm";
+    private static final String STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
+    private static final String STATUS_BAR_SHOW_BATTERY_PERCENT = "status_bar_show_battery_percent";
+    private static final String STATUS_BAR_QUICK_QS_PULLDOWN = "qs_quick_pulldown";
+
+    private static final int STATUS_BAR_BATTERY_STYLE_TEXT = 2;
+    private static final int PULLDOWN_DIR_NONE = 0;
+    private static final int PULLDOWN_DIR_RIGHT = 1;
+    private static final int PULLDOWN_DIR_LEFT = 2;
+
+    private LineageSystemSettingListPreference mQuickPulldown;
+    private LineageSystemSettingListPreference mStatusBarClock;
+    private LineageSystemSettingListPreference mStatusBarAmPm;
+    private LineageSystemSettingListPreference mStatusBarBatteryShowPercent;
+    private PreferenceCategory mStatusBarBatteryCategory;
+    private PreferenceCategory mStatusBarClockCategory;
+    private boolean mBatteryPresent;
 
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
-
         addPreferencesFromResource(R.xml.superior_lab_statusbar);
 
-        PreferenceScreen prefSet = getPreferenceScreen();
+        final Context context = getContext();
+        PreferenceScreen prefScreen = getPreferenceScreen();
 
+        mStatusBarAmPm = findPreference(STATUS_BAR_AM_PM);
+        mStatusBarClock = findPreference(STATUS_BAR_CLOCK_STYLE);
+        mStatusBarClock.setOnPreferenceChangeListener(this);
+
+        mStatusBarClockCategory = prefScreen.findPreference(CATEGORY_CLOCK);
+        mStatusBarBatteryShowPercent = findPreference(STATUS_BAR_SHOW_BATTERY_PERCENT);
+
+        LineageSystemSettingListPreference statusBarBattery =
+                findPreference(STATUS_BAR_BATTERY_STYLE);
+        statusBarBattery.setOnPreferenceChangeListener(this);
+        enableStatusBarBatteryDependents(statusBarBattery.getIntValue(2));
+
+        Intent intent = BatteryUtils.getBatteryIntent(context);
+        if (intent != null) {
+            mBatteryPresent = intent.getBooleanExtra(BatteryManager.EXTRA_PRESENT, true);
+        }
+
+        mStatusBarBatteryCategory = prefScreen.findPreference(CATEGORY_BATTERY);
+        mQuickPulldown = findPreference(STATUS_BAR_QUICK_QS_PULLDOWN);
+        mQuickPulldown.setOnPreferenceChangeListener(this);
+        updateQuickPulldownSummary(mQuickPulldown.getIntValue(0));
     }
 
     @Override
-    public boolean onPreferenceChange(Preference preference, Object objValue) {
+    public void onResume() {
+        super.onResume();
 
-        return false;
+        final String curIconBlacklist = Settings.Secure.getString(
+                getContext().getContentResolver(), ICON_BLACKLIST);
+
+        if (TextUtils.delimitedStringContains(curIconBlacklist, ',', "clock")) {
+            getPreferenceScreen().removePreference(mStatusBarClockCategory);
+        } else {
+            getPreferenceScreen().addPreference(mStatusBarClockCategory);
+        }
+
+        if (!mBatteryPresent ||
+                TextUtils.delimitedStringContains(curIconBlacklist, ',', "battery")) {
+            getPreferenceScreen().removePreference(mStatusBarBatteryCategory);
+        } else {
+            getPreferenceScreen().addPreference(mStatusBarBatteryCategory);
+        }
+
+        if (DateFormat.is24HourFormat(getActivity())) {
+            mStatusBarAmPm.setEnabled(false);
+            mStatusBarAmPm.setSummary(R.string.status_bar_am_pm_info);
+        }
+
+        final boolean disallowCenteredClock = DeviceUtils.hasCenteredCutout(getActivity());
+
+        // Adjust status bar preferences for RTL
+        if (getResources().getConfiguration().getLayoutDirection() == View.LAYOUT_DIRECTION_RTL) {
+            if (disallowCenteredClock) {
+                mStatusBarClock.setEntries(R.array.status_bar_clock_position_entries_notch_rtl);
+                mStatusBarClock.setEntryValues(R.array.status_bar_clock_position_values_notch);
+            } else {
+                mStatusBarClock.setEntries(R.array.status_bar_clock_position_entries_rtl);
+                mStatusBarClock.setEntryValues(R.array.status_bar_clock_position_values);
+            }
+            mQuickPulldown.setEntries(R.array.status_bar_quick_qs_pulldown_entries_rtl);
+        } else {
+            if (disallowCenteredClock) {
+                mStatusBarClock.setEntries(R.array.status_bar_clock_position_entries_notch);
+                mStatusBarClock.setEntryValues(R.array.status_bar_clock_position_values_notch);
+            } else {
+                mStatusBarClock.setEntries(R.array.status_bar_clock_position_entries);
+                mStatusBarClock.setEntryValues(R.array.status_bar_clock_position_values);
+            }
+            mQuickPulldown.setEntries(R.array.status_bar_quick_qs_pulldown_entries);
+        }
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        int value = Integer.parseInt((String) newValue);
+        String key = preference.getKey();
+
+        switch (key) {
+            case STATUS_BAR_QUICK_QS_PULLDOWN:
+                updateQuickPulldownSummary(value);
+                break;
+            case STATUS_BAR_CLOCK_STYLE:
+                break;
+            case STATUS_BAR_BATTERY_STYLE:
+                enableStatusBarBatteryDependents(value);
+                break;
+        }
+        return true;
+    }
+
+    private void enableStatusBarBatteryDependents(int batteryIconStyle) {
+        mStatusBarBatteryShowPercent.setEnabled(batteryIconStyle != STATUS_BAR_BATTERY_STYLE_TEXT);
+    }
+
+    private void updateQuickPulldownSummary(int value) {
+        String summary = "";
+        switch (value) {
+            case PULLDOWN_DIR_NONE:
+                summary = getResources().getString(
+                        R.string.status_bar_quick_qs_pulldown_off);
+                break;
+            case PULLDOWN_DIR_LEFT:
+            case PULLDOWN_DIR_RIGHT:
+                summary = getResources().getString(
+                        R.string.status_bar_quick_qs_pulldown_summary,
+                        getResources().getString(
+                                ((value == PULLDOWN_DIR_LEFT) ^
+                                (getResources().getConfiguration().getLayoutDirection()
+                                        == View.LAYOUT_DIRECTION_RTL))
+                                ? R.string.status_bar_quick_qs_pulldown_summary_left
+                                : R.string.status_bar_quick_qs_pulldown_summary_right));
+                break;
+        }
+        mQuickPulldown.setSummary(summary);
     }
 
     @Override
     public int getMetricsCategory() {
         return MetricsProto.MetricsEvent.SUPERIOR;
     }
-
 }
