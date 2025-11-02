2a8c88cebfac3b349fe4f0193e305c8158ede2a2
Hexaboo <hexaboo@outlook.com>
2025-11-02 07:35:38 +0100

    SuperiorLab: Import Button settings from LineageParts
    Change-Id: I1c37a5501c775b6b3137abb4a798df497f6d5465

--- src/com/superior/lab/fragments/PowerMenuSettings.java
+++ src/com/superior/lab/fragments/PowerMenuSettings.java
@@ -16,55 +16,222 @@
 
 package com.superior.lab.fragments;
 
+import static org.lineageos.internal.util.PowerMenuConstants.*;
+
+import android.Manifest;
 import android.content.Context;
-import android.content.ContentResolver;
-import android.content.Intent;
 import android.content.pm.UserInfo;
 import android.os.Bundle;
 import android.os.UserHandle;
 import android.os.UserManager;
-import androidx.preference.ListPreference;
+import android.provider.Settings;
+import android.service.controls.ControlsProviderService;
+import androidx.preference.CheckBoxPreference;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceCategory;
-import androidx.preference.PreferenceScreen;
-import androidx.preference.Preference.OnPreferenceChangeListener;
-import androidx.preference.SwitchPreference;
-import android.provider.Settings;
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.internal.util.EmergencyAffordanceManager;
 import com.android.settings.R;
-import androidx.annotation.NonNull;
-
-import com.android.internal.logging.nano.MetricsProto;
 import com.android.settings.SettingsPreferenceFragment;
-
-import java.util.Arrays;
-import java.util.ArrayList;
+import com.android.settingslib.applications.ServiceListing;
 import java.util.List;
+import lineageos.app.LineageGlobalActions;
+import org.lineageos.internal.util.PowerMenuConstants;
+import com.superior.lab.utils.TelephonyUtils;
 
+public class PowerMenuSettings extends SettingsPreferenceFragment {
+  static final String TAG = "PowerMenuSettings";
 
-public class PowerMenuSettings extends SettingsPreferenceFragment
-                implements Preference.OnPreferenceChangeListener {
+  private static final String CATEGORY_POWER_MENU_ITEMS = "superior_lab_power";
 
+  private PreferenceCategory mPowerMenuItemsCategory;
 
-    @Override
-    public void onCreate(Bundle icicle) {
-        super.onCreate(icicle);
+  private CheckBoxPreference mScreenshotPref;
+  private CheckBoxPreference mAirplanePref;
+  private CheckBoxPreference mUsersPref;
+  private CheckBoxPreference mBugReportPref;
+  private CheckBoxPreference mEmergencyPref;
+  private CheckBoxPreference mDeviceControlsPref;
 
-        addPreferencesFromResource(R.xml.superior_lab_power);
+  private LineageGlobalActions mLineageGlobalActions;
 
-        final ContentResolver resolver = getActivity().getContentResolver();
-        final PreferenceScreen prefScreen = getPreferenceScreen();
+  private EmergencyAffordanceManager mEmergencyAffordanceManager;
+  private boolean mForceEmergCheck = false;
 
+  Context mContext;
+  private UserManager mUserManager;
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    addPreferencesFromResource(R.xml.superior_lab_power);
+    requireActivity().setTitle(R.string.power_menu_title);
+    mContext = requireActivity().getApplicationContext();
+    mUserManager = UserManager.get(mContext);
+    mLineageGlobalActions = LineageGlobalActions.getInstance(mContext);
+    mEmergencyAffordanceManager = new EmergencyAffordanceManager(mContext);
+
+    mPowerMenuItemsCategory = findPreference(CATEGORY_POWER_MENU_ITEMS);
+
+    for (String action : PowerMenuConstants.getAllActions()) {
+      if (action.equals(GLOBAL_ACTION_KEY_SCREENSHOT)) {
+        mScreenshotPref = findPreference(GLOBAL_ACTION_KEY_SCREENSHOT);
+      } else if (action.equals(GLOBAL_ACTION_KEY_AIRPLANE)) {
+        mAirplanePref = findPreference(GLOBAL_ACTION_KEY_AIRPLANE);
+      } else if (action.equals(GLOBAL_ACTION_KEY_USERS)) {
+        mUsersPref = findPreference(GLOBAL_ACTION_KEY_USERS);
+      } else if (action.equals(GLOBAL_ACTION_KEY_BUGREPORT)) {
+        mBugReportPref = findPreference(GLOBAL_ACTION_KEY_BUGREPORT);
+      } else if (action.equals(GLOBAL_ACTION_KEY_EMERGENCY)) {
+        mEmergencyPref = findPreference(GLOBAL_ACTION_KEY_EMERGENCY);
+      } else if (action.equals(GLOBAL_ACTION_KEY_DEVICECONTROLS)) {
+        mDeviceControlsPref = findPreference(GLOBAL_ACTION_KEY_DEVICECONTROLS);
+      }
     }
 
-    @Override
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
+    if (!TelephonyUtils.isVoiceCapable(requireActivity())) {
+      mPowerMenuItemsCategory.removePreference(mEmergencyPref);
+      mEmergencyPref = null;
+    }
+  }
 
-        return false;
+  @Override
+  public void onStart() {
+    super.onStart();
+
+    if (mScreenshotPref != null) {
+      mScreenshotPref.setChecked(
+          mLineageGlobalActions.userConfigContains(GLOBAL_ACTION_KEY_SCREENSHOT));
     }
 
-    @Override
-    public int getMetricsCategory() {
-        return MetricsProto.MetricsEvent.SUPERIOR;
+    if (mAirplanePref != null) {
+      mAirplanePref.setChecked(
+          mLineageGlobalActions.userConfigContains(GLOBAL_ACTION_KEY_AIRPLANE));
     }
 
+    if (mUsersPref != null) {
+      if (!UserHandle.MU_ENABLED || !UserManager.supportsMultipleUsers()) {
+        mPowerMenuItemsCategory.removePreference(mUsersPref);
+        mUsersPref = null;
+      } else {
+        List<UserInfo> users = mUserManager.getUsers();
+        boolean enabled = (users.size() > 1);
+        mUsersPref.setChecked(
+            mLineageGlobalActions.userConfigContains(GLOBAL_ACTION_KEY_USERS) && enabled);
+        mUsersPref.setEnabled(enabled);
+      }
+    }
+
+    if (mBugReportPref != null) {
+      mBugReportPref.setChecked(
+          mLineageGlobalActions.userConfigContains(GLOBAL_ACTION_KEY_BUGREPORT));
+    }
+
+    if (mEmergencyPref != null) {
+      mForceEmergCheck = mEmergencyAffordanceManager.needsEmergencyAffordance();
+      mEmergencyPref.setChecked(
+          mLineageGlobalActions.userConfigContains(GLOBAL_ACTION_KEY_EMERGENCY)
+              || mForceEmergCheck);
+      mEmergencyPref.setEnabled(!mForceEmergCheck);
+    }
+
+    if (mDeviceControlsPref != null) {
+      mDeviceControlsPref.setChecked(
+          mLineageGlobalActions.userConfigContains(GLOBAL_ACTION_KEY_DEVICECONTROLS));
+
+      // Enable preference if any device control app is installed
+      ServiceListing serviceListing =
+          new ServiceListing.Builder(mContext)
+              .setIntentAction(ControlsProviderService.SERVICE_CONTROLS)
+              .setPermission(Manifest.permission.BIND_CONTROLS)
+              .setNoun("Controls Provider")
+              .setSetting("controls_providers")
+              .setTag("controls_providers")
+              .build();
+      serviceListing.addCallback(services -> mDeviceControlsPref.setEnabled(!services.isEmpty()));
+      serviceListing.reload();
+    }
+
+    updatePreferences();
+  }
+
+  @Override
+  public void onResume() {
+    super.onResume();
+    updatePreferences();
+  }
+
+  @Override
+  public boolean onPreferenceTreeClick(Preference preference) {
+    boolean value;
+
+    if (preference == mScreenshotPref) {
+      value = mScreenshotPref.isChecked();
+      mLineageGlobalActions.updateUserConfig(value, GLOBAL_ACTION_KEY_SCREENSHOT);
+
+    } else if (preference == mAirplanePref) {
+      value = mAirplanePref.isChecked();
+      mLineageGlobalActions.updateUserConfig(value, GLOBAL_ACTION_KEY_AIRPLANE);
+
+    } else if (preference == mUsersPref) {
+      value = mUsersPref.isChecked();
+      mLineageGlobalActions.updateUserConfig(value, GLOBAL_ACTION_KEY_USERS);
+
+    } else if (preference == mBugReportPref) {
+      value = mBugReportPref.isChecked();
+      mLineageGlobalActions.updateUserConfig(value, GLOBAL_ACTION_KEY_BUGREPORT);
+      Settings.Global.putInt(
+          getContentResolver(), Settings.Global.BUGREPORT_IN_POWER_MENU, value ? 1 : 0);
+
+    } else if (preference == mEmergencyPref) {
+      value = mEmergencyPref.isChecked();
+      mLineageGlobalActions.updateUserConfig(value, GLOBAL_ACTION_KEY_EMERGENCY);
+
+    } else if (preference == mDeviceControlsPref) {
+      value = mDeviceControlsPref.isChecked();
+      mLineageGlobalActions.updateUserConfig(value, GLOBAL_ACTION_KEY_DEVICECONTROLS);
+
+    } else {
+      return super.onPreferenceTreeClick(preference);
+    }
+    return true;
+  }
+
+  private void updatePreferences() {
+    UserInfo currentUser = mUserManager.getUserInfo(UserHandle.myUserId());
+    boolean developmentSettings =
+        Settings.Global.getInt(
+                getContentResolver(), Settings.Global.DEVELOPMENT_SETTINGS_ENABLED, 0)
+            == 1;
+    boolean bugReport =
+        Settings.Global.getInt(getContentResolver(), Settings.Global.BUGREPORT_IN_POWER_MENU, 0)
+            == 1;
+    boolean isPrimaryUser = currentUser == null || currentUser.isPrimary();
+    if (mBugReportPref != null) {
+      mBugReportPref.setEnabled(developmentSettings && isPrimaryUser);
+      if (!developmentSettings) {
+        mBugReportPref.setChecked(false);
+        mBugReportPref.setSummary(R.string.power_menu_bug_report_devoptions_unavailable);
+      } else if (!isPrimaryUser) {
+        mBugReportPref.setChecked(false);
+        mBugReportPref.setSummary(R.string.power_menu_bug_report_unavailable_for_user);
+      } else {
+        mBugReportPref.setChecked(bugReport);
+        mBugReportPref.setSummary(null);
+      }
+    }
+    if (mEmergencyPref != null) {
+      if (mForceEmergCheck) {
+        mEmergencyPref.setSummary(R.string.power_menu_emergency_affordance_enabled);
+      } else {
+        mEmergencyPref.setSummary(null);
+      }
+    }
+  }
+
+  @Override
+  public int getMetricsCategory() {
+    return MetricsEvent.SUPERIOR;
+  }
 }
