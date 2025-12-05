d664a4bf19fc3b4df9b5891005326352861e91c0
momenabdulrazek <momenabdulrazek@users.noreply.github.com>
2025-12-05 23:06:04 +0100

    Import Lockscreen & Buttons preferences from crDroid Settings

--- src/com/sakura/settings/fragments/LockScreenSettings.java
+++ src/com/sakura/settings/fragments/LockScreenSettings.java
@@ -1,78 +1,185 @@
 /*
- *  Copyright (C) 2022 SuperiorOS
+ * Copyright (C) 2016-2025 crDroid Android Project
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.sakura.settings.fragments;
 
-import com.android.internal.logging.nano.MetricsProto;
-
-import android.app.Activity;
-import android.content.Context;
 import android.content.ContentResolver;
-import android.app.WallpaperManager;
-import android.content.Intent;
-import android.content.res.Resources;
-import android.hardware.fingerprint.FingerprintManager;
-import android.net.Uri;
+import android.content.Context;
 import android.os.Bundle;
-import androidx.preference.SwitchPreferenceCompat;
-import androidx.preference.ListPreference;
+import android.os.UserHandle;
+import android.provider.SearchIndexableResource;
+import android.provider.Settings;
+
 import androidx.preference.Preference;
 import androidx.preference.PreferenceCategory;
-import androidx.preference.PreferenceScreen;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.SwitchPreferenceCompat;
 
-import android.provider.Settings;
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.internal.util.sakura.OmniJawsClient;
+import com.android.internal.util.sakura.Utils;
+
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
-import com.android.internal.util.sakura.OmniJawsClient;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settingslib.search.SearchIndexable;
 
-public class LockScreenSettings extends SettingsPreferenceFragment implements
-        Preference.OnPreferenceChangeListener {
+import com.sakura.settings.fragments.lockscreen.PulseSettings;
+import com.sakura.settings.fragments.lockscreen.MediaArtSettings;
+import com.sakura.settings.fragments.lockscreen.UdfpsAnimation;
+import com.sakura.settings.fragments.lockscreen.UdfpsIconPicker;
+import com.sakura.settings.utils.DeviceUtils;
+import com.sakura.settings.utils.SystemUtils;
+import com.sakura.settings.utils.TelephonyUtils;
 
+import java.util.List;
+
+@SearchIndexable
+public class LockScreenSettings extends SettingsPreferenceFragment
+            implements Preference.OnPreferenceChangeListener  {
+
+    public static final String TAG = "LockScreenSettings";
+
+    private static final String LOCKSCREEN_GESTURES_CATEGORY = "lockscreen_gestures_category";
+    private static final String LOCKSCREEN_INTERFACE_CATEGORY = "lockscreen_interface_category";
+    private static final String KEY_RIPPLE_EFFECT = "enable_ripple_effect";
+    private static final String KEY_SMARTSPACE = "lockscreen_smartspace_enabled";
     private static final String KEY_WEATHER = "lockscreen_weather_enabled";
+    private static final String KEY_UDFPS_ANIMATIONS = "udfps_recognizing_animation_preview";
+    private static final String KEY_UDFPS_ICONS = "udfps_icon_picker";
 
-    private Preference mWeather;
-    private OmniJawsClient mWeatherClient;
+    private static final String KEY_FP_SUCCESS = "fp_success_vibrate";
+    private static final String KEY_FP_ERROR = "fp_error_vibrate";
+
+    private static final String KEY_CARRIER_NAME = "lockscreen_show_carrier";
+
+    private Preference mUdfpsAnimations;
+    private Preference mUdfpsIcons;
+    private Preference mRippleEffect;
+
+    private SwitchPreferenceCompat mSmartspace;
+    private SwitchPreferenceCompat mWeather;
+    private SwitchPreferenceCompat mFpSuccessVib;
+    private SwitchPreferenceCompat mFpErrorVib;
 
     @Override
-    public void onCreate(Bundle icicle) {
-        super.onCreate(icicle);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
         addPreferencesFromResource(R.xml.sakura_settings_lockscreen);
+        final Context context = getContext();
 
-        ContentResolver resolver = getActivity().getContentResolver();
-        Resources resources = getResources();
-        final PreferenceScreen prefScreen = getPreferenceScreen();
-        final PreferenceScreen prefSet = getPreferenceScreen();
+        PreferenceCategory gestCategory = (PreferenceCategory) findPreference(LOCKSCREEN_GESTURES_CATEGORY);
 
-        mWeather = (Preference) findPreference(KEY_WEATHER);
-        mWeatherClient = new OmniJawsClient();
+        mUdfpsAnimations = (Preference) findPreference(KEY_UDFPS_ANIMATIONS);
+        mUdfpsIcons = (Preference) findPreference(KEY_UDFPS_ICONS);
+        mFpSuccessVib = findPreference(KEY_FP_SUCCESS);
+        mFpErrorVib = findPreference(KEY_FP_ERROR);
+        mRippleEffect = (Preference) findPreference(KEY_RIPPLE_EFFECT);
+
+        boolean hasFingerprint = DeviceUtils.hasFingerprint(context);
+        if (!hasFingerprint) {
+            gestCategory.removePreference(mUdfpsAnimations);
+            gestCategory.removePreference(mUdfpsIcons);
+            gestCategory.removePreference(mRippleEffect);
+        } else {
+            if (!Utils.isPackageInstalled(context, "com.sakura.udfps.animations")) {
+                gestCategory.removePreference(mUdfpsAnimations);
+            }
+            if (!Utils.isPackageInstalled(context, "com.sakura.udfps.icons")) {
+                gestCategory.removePreference(mUdfpsIcons);
+            }
+        }
+
+        boolean hapticAvailable = DeviceUtils.hasVibrator(context);
+        if (!hasFingerprint || !hapticAvailable) {
+            gestCategory.removePreference(mFpSuccessVib);
+            gestCategory.removePreference(mFpErrorVib);
+        }
+
+        if (!TelephonyUtils.isVoiceCapable(context)) {
+            PreferenceCategory intCategory = (PreferenceCategory) findPreference(LOCKSCREEN_INTERFACE_CATEGORY);
+            SwitchPreferenceCompat carrierName = findPreference(KEY_CARRIER_NAME);
+            intCategory.removePreference(carrierName);
+        }
+
+        mSmartspace = (SwitchPreferenceCompat) findPreference(KEY_SMARTSPACE);
+        mSmartspace.setOnPreferenceChangeListener(this);
+
+        mWeather = (SwitchPreferenceCompat) findPreference(KEY_WEATHER);
+        mWeather.setOnPreferenceChangeListener(this);
+
         updateWeatherSettings();
     }
 
+    @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mSmartspace) {
+            mSmartspace.setChecked((Boolean)newValue);
+            updateWeatherSettings();
+            SystemUtils.showSystemUiRestartDialog(getContext());
+            return true;
+        } else if (preference == mWeather) {
+            mWeather.setChecked((Boolean)newValue);
+            SystemUtils.showSystemUiRestartDialog(getContext());
+            return true;
+        }
 
         return false;
     }
 
+    public static void reset(Context mContext) {
+        ContentResolver resolver = mContext.getContentResolver();
+        Settings.Secure.putIntForUser(resolver,
+                Settings.Secure.LOCKSCREEN_SMARTSPACE_ENABLED, 1, UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(resolver,
+                Settings.System.LOCKSCREEN_BATTERY_INFO, 1, UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(resolver,
+                Settings.System.DOUBLE_TAP_SLEEP_LOCKSCREEN, 1, UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(resolver,
+                Settings.System.ENABLE_RIPPLE_EFFECT, 1, UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(resolver,
+                Settings.System.FP_ERROR_VIBRATE, 1, UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(resolver,
+                Settings.System.FP_SUCCESS_VIBRATE, 1, UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(resolver,
+                Settings.System.LOCKSCREEN_ENABLE_POWER_MENU, 1, UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(resolver,
+                Settings.System.LOCKSCREEN_WEATHER_ENABLED, 0, UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(resolver,
+                Settings.System.LOCKSCREEN_WEATHER_LOCATION, 0, UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(resolver,
+                Settings.System.LOCKSCREEN_WEATHER_TEXT, 1, UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(resolver,
+                Settings.System.LOCKSCREEN_WEATHER_WIND_INFO, 0, UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(resolver,
+                Settings.System.LOCKSCREEN_WEATHER_HUMIDITY_INFO, 0, UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(resolver,
+                Settings.System.LOCKSCREEN_SHOW_CARRIER, 1, UserHandle.USER_CURRENT);
+        PulseSettings.reset(mContext);
+        MediaArtSettings.reset(mContext);
+        UdfpsAnimation.reset(mContext);
+        UdfpsIconPicker.reset(mContext);
+    }
+
     private void updateWeatherSettings() {
-        if (mWeatherClient == null || mWeather == null) return;
-        boolean weatherEnabled = mWeatherClient.isOmniJawsEnabled(getContext());
-        mWeather.setEnabled(weatherEnabled);
-        mWeather.setSummary(weatherEnabled ? R.string.lockscreen_weather_summary :
+        if (mWeather == null || mSmartspace == null) return;
+
+        boolean weatherEnabled = OmniJawsClient.get().isOmniJawsEnabled(getContext());
+        mWeather.setEnabled(!mSmartspace.isChecked() && weatherEnabled);
+        mWeather.setSummary(!mSmartspace.isChecked() && weatherEnabled ? R.string.lockscreen_weather_summary :
             R.string.lockscreen_weather_enabled_info);
     }
 
@@ -87,4 +194,38 @@ public class LockScreenSettings extends SettingsPreferenceFragment implements
         return MetricsProto.MetricsEvent.SAKURA_SETTINGS;
     }
 
+    /**
+     * For search
+     */
+    public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+            new BaseSearchIndexProvider(R.xml.sakura_settings_lockscreen) {
+
+                @Override
+                public List<String> getNonIndexableKeys(Context context) {
+                    List<String> keys = super.getNonIndexableKeys(context);
+
+                    boolean hasFingerprint = DeviceUtils.hasFingerprint(context);
+                    if (!hasFingerprint) {
+                        keys.add(KEY_UDFPS_ANIMATIONS);
+                        keys.add(KEY_UDFPS_ICONS);
+                        keys.add(KEY_RIPPLE_EFFECT);
+                    } else {
+                        if (!Utils.isPackageInstalled(context, "com.sakura.udfps.animations")) {
+                            keys.add(KEY_UDFPS_ANIMATIONS);
+                        }
+                        if (!Utils.isPackageInstalled(context, "com.sakura.udfps.icons")) {
+                            keys.add(KEY_UDFPS_ICONS);
+                        }
+                    }
+                    boolean hapticAvailable = DeviceUtils.hasVibrator(context);
+                    if (!hasFingerprint || !hapticAvailable) {
+                        keys.add(KEY_FP_SUCCESS);
+                        keys.add(KEY_FP_ERROR);
+                    }
+                    if (!TelephonyUtils.isVoiceCapable(context)) {
+                        keys.add(KEY_CARRIER_NAME);
+                    }
+                    return keys;
+                }
+            };
 }
