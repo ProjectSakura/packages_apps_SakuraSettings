42b9f59a28bab11999cee8cf880a49122cbb197c
momenabdulrazek <momenabdulrazek@users.noreply.github.com>
2026-03-16 02:33:13 +0100

    QuickSettings: remove bluetooth dialog toggle
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: Mo'men Abdulrazek <momen.abdelrazek33@gmail.com>

--- src/com/sakura/settings/fragments/QuickSettings.java
+++ src/com/sakura/settings/fragments/QuickSettings.java
@@ -135,8 +135,6 @@ public class QuickSettings extends SettingsPreferenceFragment implements
         Settings.System.putIntForUser(resolver,
                 Settings.System.QS_BRIGHTNESS_SLIDER_SHAPE, 0, UserHandle.USER_CURRENT);
         Settings.System.putIntForUser(resolver,
-                Settings.System.QS_BT_SHOW_DIALOG, 1, UserHandle.USER_CURRENT);
-        Settings.System.putIntForUser(resolver,
                 Settings.System.QS_SHOW_BATTERY_PERCENT, 2, UserHandle.USER_CURRENT);
         Settings.System.putIntForUser(resolver,
                 Settings.System.QS_SHOW_DATA_USAGE, 0, UserHandle.USER_CURRENT);
