c8d8b61b689b9f104a30acf027ed544ba6580bee
momenabdulrazek <momenabdulrazek@users.noreply.github.com>
2026-03-18 00:04:32 +0100

    SakuraSettings: Disable some statusbar/qs battery customization
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: Mo'men Abdulrazek <momen.abdelrazek33@gmail.com>

--- src/com/sakura/settings/fragments/QuickSettings.java
+++ src/com/sakura/settings/fragments/QuickSettings.java
@@ -129,8 +129,6 @@ public class QuickSettings extends SettingsPreferenceFragment implements
         Settings.Secure.putIntForUser(resolver,
                 Settings.Secure.ENABLE_LOCKSCREEN_QUICK_SETTINGS, 1, UserHandle.USER_CURRENT);
         Settings.System.putIntForUser(resolver,
-                Settings.System.QS_BATTERY_STYLE, -1, UserHandle.USER_CURRENT);
-        Settings.System.putIntForUser(resolver,
                 Settings.System.QS_BRIGHTNESS_SLIDER_HAPTIC, 0, UserHandle.USER_CURRENT);
         Settings.System.putIntForUser(resolver,
                 Settings.System.QS_BRIGHTNESS_SLIDER_SHAPE, 0, UserHandle.USER_CURRENT);
@@ -140,8 +138,6 @@ public class QuickSettings extends SettingsPreferenceFragment implements
                 Settings.System.QS_FOOTER_SHOW_EDIT, 1, UserHandle.USER_CURRENT);
         Settings.System.putIntForUser(resolver,
                 Settings.System.QS_FOOTER_SHOW_POWER_MENU, 1, UserHandle.USER_CURRENT);
-        Settings.System.putIntForUser(resolver,
-                Settings.System.QS_SHOW_BATTERY_PERCENT, 2, UserHandle.USER_CURRENT);
         Settings.System.putIntForUser(resolver,
                 Settings.System.QS_SHOW_DATA_USAGE, 0, UserHandle.USER_CURRENT);
         Settings.System.putIntForUser(resolver,
