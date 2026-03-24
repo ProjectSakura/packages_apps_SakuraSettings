f287a6fd98730f4aa524e8d09849b263fc08d669
momenabdulrazek <momenabdulrazek@users.noreply.github.com>
2026-03-24 23:02:38 +0100

    Import Sound Settings
    All credits goes to crDroid Team !

---
+++ src/com/sakura/settings/fragments/SoundSettings.java
@@ -0,0 +1,156 @@
+/*
+ * Copyright (C) 2016-2025 crDroid Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.sakura.settings.fragments;
+
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.SystemProperties;
+import android.os.UserHandle;
+import android.provider.Settings;
+
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.SwitchPreferenceCompat;
+
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settingslib.search.SearchIndexable;
+
+import com.sakura.settings.utils.DeviceUtils;
+import com.sakura.settings.utils.TelephonyUtils;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import lineageos.providers.LineageSettings;
+
+@SearchIndexable
+public class SoundSettings extends SettingsPreferenceFragment {
+
+    public static final String TAG = "Sound";
+
+    private static final String KEY_VOLUME_CATEGORY = "volume_panel";
+    private static final String KEY_VIBRATE_CATEGORY = "incall_vib_options";
+    private static final String KEY_VIBRATE_CONNECT = "vibrate_on_connect";
+    private static final String KEY_VIBRATE_CALLWAITING = "vibrate_on_callwaiting";
+    private static final String KEY_VIBRATE_DISCONNECT = "vibrate_on_disconnect";
+    private static final String KEY_VOLUME_PANEL_LEFT = "volume_panel_on_left";
+    private static final String KEY_VOLUME_HAPTIC = "volume_dialog_haptic_feedback";
+
+    private SwitchPreferenceCompat mVolumePanelLeft;
+    private SwitchPreferenceCompat mVolumeHaptic;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.sakura_settings_sound);
+
+        final Context context = getContext();
+        final PreferenceScreen prefScreen = getPreferenceScreen();
+
+        boolean isAudioPanelOnLeft = LineageSettings.Secure.getIntForUser(context.getContentResolver(),
+                LineageSettings.Secure.VOLUME_PANEL_ON_LEFT, isAudioPanelOnLeftSide(context) ? 1 : 0,
+                UserHandle.USER_CURRENT) != 0;
+
+        mVolumePanelLeft = prefScreen.findPreference(KEY_VOLUME_PANEL_LEFT);
+        mVolumePanelLeft.setChecked(isAudioPanelOnLeft);
+
+        boolean voiceCapable = TelephonyUtils.isVoiceCapable(context);
+        boolean hapticAvailable = DeviceUtils.hasVibrator(context);
+
+        if (!voiceCapable || !hapticAvailable) {
+            final PreferenceCategory vibCategory = prefScreen.findPreference(KEY_VIBRATE_CATEGORY);
+            prefScreen.removePreference(vibCategory);
+        }
+        if (!hapticAvailable) {
+            final PreferenceCategory volCategory = prefScreen.findPreference(KEY_VOLUME_CATEGORY);
+            mVolumeHaptic = volCategory.findPreference(KEY_VOLUME_HAPTIC);
+            volCategory.removePreference(mVolumeHaptic);
+        }
+    }
+
+    public static void reset(Context mContext) {
+        ContentResolver resolver = mContext.getContentResolver();
+        LineageSettings.Secure.putIntForUser(resolver,
+                LineageSettings.Secure.VOLUME_PANEL_ON_LEFT, isAudioPanelOnLeftSide(mContext) ? 1 : 0,
+                UserHandle.USER_CURRENT);
+        Settings.Secure.putIntForUser(resolver,
+                Settings.Secure.VOLUME_DIALOG_DISMISS_TIMEOUT, 3000, UserHandle.USER_CURRENT);
+        Settings.Secure.putIntForUser(resolver,
+                Settings.Secure.VOLUME_DIALOG_HAPTIC_FEEDBACK, 1, UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(resolver,
+                Settings.System.VIBRATE_ON_CONNECT, 0, UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(resolver,
+                Settings.System.VIBRATE_ON_CALLWAITING, 0, UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(resolver,
+                Settings.System.VIBRATE_ON_DISCONNECT, 0, UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(resolver,
+                Settings.System.SCREENSHOT_SHUTTER_SOUND, 1, UserHandle.USER_CURRENT);
+    }
+
+    private static boolean isAudioPanelOnLeftSide(Context context) {
+        try {
+            Context con = context.createPackageContext("org.lineageos.lineagesettings", 0);
+            int id = con.getResources().getIdentifier("def_volume_panel_on_left",
+                    "bool", "org.lineageos.lineagesettings");
+            if (id <= 0) return false;
+            return con.getResources().getBoolean(id);
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.SAKURA_SETTINGS;
+    }
+
+    /**
+     * For search
+     */
+    public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+            new BaseSearchIndexProvider(R.xml.sakura_settings_sound) {
+
+                @Override
+                public List<String> getNonIndexableKeys(Context context) {
+                    List<String> keys = super.getNonIndexableKeys(context);
+
+                    boolean voiceCapable = TelephonyUtils.isVoiceCapable(context);
+                    boolean hapticAvailable = DeviceUtils.hasVibrator(context);
+
+                    if (!voiceCapable || !hapticAvailable) {
+                        keys.add(KEY_VIBRATE_CATEGORY);
+                        keys.add(KEY_VIBRATE_CONNECT);
+                        keys.add(KEY_VIBRATE_CALLWAITING);
+                        keys.add(KEY_VIBRATE_DISCONNECT);
+                    }
+                    if (!hapticAvailable) {
+                        keys.add(KEY_VOLUME_HAPTIC);
+                    }
+                    return keys;
+                }
+            };
+}
